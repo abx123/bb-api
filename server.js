2adf7c0fdf403edf70f158f3b7513a71e037830f
Wing Ming Sam <wmsam91@gmail.com>
2020-11-27 15:43:53 +0800

    test

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.post('/test', (req, res) => {
     let date = subCmd[1].split(/ (.+)/)
     let test = date[0] + '/2020'
     let slackReq = {
-        text: ':bb-here::bb-who-find:\n ' + cmd[0] + '\n ' + subCmd[0] + '\n' + Date.parse(date[0] + '/2020') + '\n' + test
+        text: ':bb-here::bb-who-find:\n ' + cmd[0] + '\n ' + subCmd[0] + '\n' + Date.parse(date[0] + '/' + new Date().getFullYear()) + '\n' + test
     }
     if (req.body.event.channel === bb2) {
         axios.post('https://hooks.slack.com/services/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', JSON.stringify(slackReq))
