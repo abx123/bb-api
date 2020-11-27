e1c60ee17599cc983ab0341c33aa0e4956153960
Wing Ming Sam <wmsam91@gmail.com>
2020-11-27 15:06:47 +0800

    test

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const axios = require('axios')
 const app = express();
 const port = process.env.PORT || 8000
 
+const bb2 = 'G01FLHXFZTM'
 
 app.use(express.json())
 
@@ -13,11 +14,13 @@ app.get('/', (req, res) => {
 app.post('/test', (req, res) => {
     axios.post('https://novel-fac48.firebaseio.com/test.json', req.body.event)
         .then()
-        // let slackReq = { text: req.body.event.text.substring(15), }
-    let slackReq = { text: ':bb-here::bb-who-find:' }
-    axios.post('https://hooks.slack.com/services/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', JSON.stringify(slackReq))
-        .then()
-    res.send(req.body);
+        // let cmd = { text: req.body.event.text.substring(15).split(' ') }
+    let slackReq = { text: ':bb-here::bb-who-find:' + req.body.event.text.substring(15).split(' ')[0] }
+    if (req.body.event.channel === bb2) {
+        axios.post('https://hooks.slack.com/services/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', JSON.stringify(slackReq))
+            .then()
+        res.send(req.body);
+    }
 });
 app.listen(port, () => {
     console.log('Example app listening on port ' + port + '!')
