e772a72c0fbc3d04147442eb65491003759ffc22
Wing Ming Sam <wmsam91@gmail.com>
2020-11-27 15:32:48 +0800

    test

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.get('/', (req, res) => {
 app.post('/test', (req, res) => {
     axios.post('https://novel-fac48.firebaseio.com/test.json', req.body.event)
         .then()
-    let cmd = req.body.event.text.substring(15).split(/ (.+)/)[0]
-    let subCmd = cmd.substring(15).split(/ (.+)/)[0]
+    let cmd = req.body.event.text.substring(15).split(/ (.+)/)
+    let subCmd = cmd[1].substring(15).split(/ (.+)/)
     let slackReq = {
-        text: ':bb-here::bb-who-find:\n ' + cmd + '\n ' + subCmd + '/2020' + Date.parse(subCmd + '/2020')
+        text: ':bb-here::bb-who-find:\n ' + cmd[0] + '\n ' + subCmd[0] + '/2020' + Date.parse(subCmd[0] + '/2020')
     }
     if (req.body.event.channel === bb2) {
         axios.post('https://hooks.slack.com/services/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', JSON.stringify(slackReq))
