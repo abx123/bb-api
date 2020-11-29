77d9866c5a20e96eb6ed9e98118f2a825987d19c
Wing Ming Sam <wmsam91@gmail.com>
2020-11-29 22:05:36 +0800

    test

--- server.js
+++ server.js
@@ -15,42 +15,16 @@ app.post('/test', (req, res) => {
     axios.post('https://novel-fac48.firebaseio.com/test.json', req.body.event)
         .then()
     let cmd = req.body.event.text.substring(15).split(/ (.+)/)
-    if (cmd.length === 2) {
-        let subCmd = cmd[1].split(/ (.+)/)
-        if (subCmd.length === 2) {
-            let date = subCmd[1].split(/ (.+)/)
-            let test = date[0] + '/2020'
-            let slackReq = {
-                text: ':bb-here::bb-who-find:\n ' + cmd[0] + '\n ' + subCmd[0] + '\n' + Date.parse(date[0] + '/' + new Date().getFullYear()) + '\n' + test
-            }
-            if (req.body.event.channel === bb2) {
-                axios.post('https://hooks.slack.com/services/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', JSON.stringify(slackReq))
-                res.send(req.body);
-            }
-        }
-
-
+    let subCmd = cmd[1].split(/ (.+)/)
+    let date = subCmd[1].split(/ (.+)/)
+    let test = date[0] + '/2020'
+    let slackReq = {
+        text: ':bb-here::bb-who-find:\n ' + cmd[0] + '\n ' + subCmd[0] + '\n' + Date.parse(date[0] + '/' + new Date().getFullYear()) + '\n' + test
     }
-    // cmdtxt = req.body.event.text.substring(15)
-    // let command = cmdtxt.split(' ')
-    // let replyObj = { text: ':bb-no-understand:' }
-    // switch (command.length) {
-    //     case 1:
-    //         replyObj.text = 'command:' + command[0]
-    //         break;
-    //     case 2:
-    //         replyObj.text = 'command:' + command[0] + '\n subcmd:' + command[1]
-    //         break;
-    //     case 3:
-    //         replyObj.text = 'command:' + command[0] + '\n subcmd:' + command[1] + '\n date:' + command[2]
-    //         break;
-    //     case command.length > 3:
-    //         var prefix = cmdtxt.match(/(.*?\s){3}/g)
-    //         replyObj.text = 'command:' + command[0] + '\n subcmd:' + command[1] + '\n date:' + command[2] + '\n text:' + cmdtxt.substring(prefix.length)
-    //         break;
-    //     default:
-    //         break;
-    // }
+    if (req.body.event.channel === bb2) {
+        axios.post('https://hooks.slack.com/services/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', JSON.stringify(slackReq))
+        res.send(req.body);
+    }
 });
 app.listen(port, () => {
     console.log('Example app listening on port ' + port + '!')
