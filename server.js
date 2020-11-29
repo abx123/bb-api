394e89fbba838a78f482526d5f02dadc1062325e
Wing Ming Sam <wmsam91@gmail.com>
2020-11-29 22:30:06 +0800

    v2

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.post('/test', (req, res) => {
     let replyObj = { text: ':bb-no-understand:' }
     switch (true) {
         case (command.length == 1):
+            console.log('command[0]:' + '!')
             replyObj.text = 'command:' + command[0]
             if (command[0] = "") {
                 replyObj.text = ':bb-here::bb-who-find:'
